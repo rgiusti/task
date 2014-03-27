1983e578a20a16fd76f2e657da68dd459df51394
rafael giusti <rafaelgiusti2002@rafaels-iMac-2.local>
2014-03-27 10:29:44 -0400

    Initial Commit

---
+++ task2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  task2
+//
+//  Created by rafael giusti on 3/27/14.
+//  Copyright (c) 2014 rafael giusti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
