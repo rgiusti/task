434349c8e797a7a42c47207249ca22f93dba16d9
rafael giusti <rafaelgiusti2002@rafaels-iMac-2.local>
2014-03-27 12:22:05 -0400

    Testing

--- task2/main.c
+++ task2/main.c
@@ -7,12 +7,40 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("x^y mod z: \n");
+    
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+    int z = atoi(argv[3]);
+    
+    printf("x= %d, y=%d, z=%d \n", x, y, z);
+    
+    double xy_test = pow(x,y);
+    printf("x^y = %f\n", xy_test);
+    int remainder_test = (int)(xy_test)%z;
+    printf("test remainder (x^y) mod z)= %d\n\n", remainder_test);
+    
+    
+    int remainder = x;
+    int x_Pow_y;
+    
+    for (int i=0; i<y; i++) {
+        x_Pow_y *= x_Pow_y;
+    }
+    
+    
+    int xy_by_z = x_Pow_y/z;
+    printf("xy/z = %d\n", xy_by_z);
+    int xy_by_z_times_z = xy_by_z*z;
+    printf("xy_by_z_times_z= %d\n",xy_by_z_times_z);
+    
+    
+    printf("remainder (x^y) mod z)= %d\n", remainder);
+    
     return 0;
 }
 
